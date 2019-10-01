8636244a3534b96b4fa86858be2b7e94b3ea8b0b
Liam Toney <ldtoney@alaska.edu>
2019-10-01 11:10:29 -0800

    Simplify setup.py
    This distills it to the bare minimum. If we decide to upload to PyPI or
    elsewhere, we can add in relevant package info...

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
-config = {
-    'description':      'Collect seismic or infrasound waveform data and metadata from IRIS, WATC, or AVO servers or from local file sources.',
-    'name':             'waveform_collection',
-    'author':           'David Fee, Liam Toney, Andrew Winkelman',
-    'author_email':     'dfee1@alaska.edu, ldtoney@alaska.edu, atwinkelman@alaska.edu',
-    'packages':         ['waveform_collection'],
-    'install_requires': ['numpy', 'obspy'],
-    'data_files':       [('avo_json', ['avo_json/avo_infra_calibs.json', 'avo_json/avo_infra_coords.json'])]
-    }
-
-setup(**config)
+setup(
+      name='waveform_collection',
+      packages=find_packages()
+      )
