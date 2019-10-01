e292b1a108e5e99ee8155941d0753af2dd7d875e
Liam Toney <toney.liam@gmail.com>
2019-10-01 00:09:30 -0800

    Add deps back in for pure pip users

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ config = {
     'author':           'David Fee, Liam Toney, Andrew Winkelman',
     'author_email':     'dfee1@alaska.edu, ldtoney@alaska.edu, atwinkelman@alaska.edu',
     'packages':         ['waveform_collection'],
+    'install_requires'  ['numpy', 'obspy']
     'data_files':       [('avo_json', ['avo_json/avo_infra_calibs.json', 'avo_json/avo_infra_coords.json'])]
     }
 
