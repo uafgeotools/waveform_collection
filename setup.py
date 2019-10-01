7ca0e9b9558b0041427bfce9e276b02fa835f2c6
Liam Toney <ldtoney@alaska.edu>
2019-09-30 17:07:35 -0800

    Remove version number as it has no meaning

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ config = {
     'name':             'waveform_collection',
     'author':           'David Fee, Liam Toney, Andrew Winkelman',
     'author_email':     'dfee1@alaska.edu, ldtoney@alaska.edu, atwinkelman@alaska.edu',
-    'version':          '0.1',
     'install_requires': ['obspy'],
     'scripts':          ['waveform_collection.py'],
     'data_files':       [('avo_json',['avo_json/avo_infra_calibs.json','avo_json/avo_infra_coords.json'])]
