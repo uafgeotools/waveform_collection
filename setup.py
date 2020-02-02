06cf31c153dfaeb8eb3a625a232c26ac45c54219
Liam Toney <toney.liam@gmail.com>
2020-02-02 13:11:46 -0900

    Include AVO JSON files if on RTD

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ import os
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
     INSTALL_REQUIRES = []
+    INCLUDE_PACKAGE_DATA = True
 else:
     INSTALL_REQUIRES = ['obspy']
+    INCLUDE_PACKAGE_DATA = False
 
 setup(
       name='waveform_collection',
       packages=find_packages(),
-      install_requires=INSTALL_REQUIRES
+      install_requires=INSTALL_REQUIRES,
+      package_data={'': ['../avo_json/*.json']},
+      include_package_data=INCLUDE_PACKAGE_DATA
       )
